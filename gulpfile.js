aa04f390dbfa1cba9797124eefda233bd9421810
Kaz <chgm.bunbuker@gmail.com>
2018-09-13 11:16:50 -0700

    adding gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+// dependencies
+var del = require('del');
+var express = require('express');
+var log = require('fancy-log');
+var watchify = require('watchify');
+
+// gulp dependencies
+var gulp = require('gulp');
+var autoprefixer = require('gulp-autoprefixer');
+var concat = require('gulp-concat');
+var connect = require('gulp-connect');
+var gutil = require('gulp-util');
+var sass = require('gulp-sass');
+var plumber = require('gulp-plumber');
+var watch = require('gulp-watch');
+
+// constructors
+var app = express();
+var http = require('http').Server(app);
+app.use( express.static('./public') );
+
+// asset copy location maps
+var copyLocations = [
+  {
+    src: './src/assets/**/*.*',
+    dest: './public/assets'
+  },
+  {
+    src: './src/index.html',
+    dest: './public'
+  },
+  {
+    src: './src/vendor/**/*.*',
+    dest: './public/vendor'
+  }
+];
+
+var watchLocations = {
+  sass: './src/sass/**/*.scss',
+  js: './src/js/**/*.js'
+};
+
+var destLocations = {
+  sass: './public/css'
+};
+
+/* ERROR HANDLER */
+var onError = function(err) {
+  gutil.log(gutil.colors.red('ERROR', err.plugin), err.message);
+  gutil.beep();
+  new gutil.PluginError(err.plugin, err, {showStack: true});
+};
+
+/* CLEAN */
+gulp.task('clean', function(cb) {
+  return del(['public/**/*'], cb);
+});
+
+/* COPY */
+gulp.task('copy', function(cb) {
+  var task;
+  for (var i = 0; i < copyLocations.length; i++) {
+    task = gulp.src(copyLocations[i].src)
+    .pipe(plumber())
+    .pipe(watch(copyLocations[i].src))
+    .pipe(gulp.dest(copyLocations[i].dest))
+    .pipe(connect.reload());
+  }
+  cb();
+});
+
+/* WATCH DATA */
+gulp.task('data:watch', function() {
+  watch(watchLocations.data, function() {
+    io.emit('data.refresh', true);
+  });
+});
+
+/* SASS */
+gulp.task('sass', function() {
+  gulp.src('src/sass/**/*.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(autoprefixer())
+      .pipe(gulp.dest('./public/css'))
+      .pipe(connect.reload());
+});
+
+gulp.task('sass:watch', function() {
+  watch(watchLocations.sass, function() {
+    gulp.start('sass');
+  });
+});
+
+gulp.task('scripts', function() {
+  gulp.src('./src/js/**/*.*')
+      .pipe(concat('stack.js'))
+      .pipe(gulp.dest('./public/js'))
+      .pipe(connect.reload());
+});
+
+gulp.task('scripts:watch', function() {
+  watch(watchLocations.js, function() {
+    gulp.start('scripts');
+  });
+});
+
+/* SERVER */
+gulp.task('server', function() {
+  http.listen(5050, function() {
+    log.info('Server Started!! Listening on port: 5050')
+  });
+});
+
+/* MAIN TASKS */
+gulp.task('dev', ['clean'], function() {
+  gulp.start('copy');
+  gulp.start('sass');
+  gulp.start('sass:watch');
+  gulp.start('scripts');
+  gulp.start('scripts:watch');
+  gulp.start('server');
+});
